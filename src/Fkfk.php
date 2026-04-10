7237be3196221322eee4efc8475cecd3a38a8b17
Muhamad-SazidanA <muhamadsazidanal-zahran@smkwikrama.sch.id>
2026-04-10 17:12:55 +0700

    feat: add base FKFK package class and autoload

---
+++ src/Fkfk.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SazidanA\Fkfk;
+
+final class Fkfk
+{
+    public const VERSION = '1.0.0';
+
+    public static function version(): string
+    {
+        return self::VERSION;
+    }
+}
